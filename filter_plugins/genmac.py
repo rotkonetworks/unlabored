08bd0e44862dd6f2eca20fa6a216f39c7c5ac540
hitchhooker <tommi@niemi.lol>
2023-08-07 05:24:31 +0700

    flake8 fix

--- filter_plugins/genmac.py
+++ filter_plugins/genmac.py
@@ -7,7 +7,7 @@ def generate_mac(hostname):
     md5.update(hostname.encode('utf-8'))
     digest = md5.digest()
     mac = binascii.hexlify(digest)[:12]
-    mac = ':'.join([mac[i:i+2] for i in range(0, len(mac), 2)])
+    mac = ':'.join([mac[i:i + 2] for i in range(0, len(mac), 2)])
     mac = '69' + mac[2:]  # modify first octet to make it unicast
     return mac.decode('utf-8')
 
--- filter_plugins/routeros_filters.py
+++ filter_plugins/routeros_filters.py
@@ -1,12 +1,3 @@
-# filter_plugins/routeros_filters.py
-
-# 36    chain=dstnat action=dst-nat to-addresses=192.168.69.121 to-ports=33121 protocol=tcp in-interface=ether1 dst-port=33121
-# 3    chain=dstnat action=dst-nat to-addresses=192.168.69.121 to-ports=33121 protocol=tcp dst-address=27.131.160.106 dst-port=33121
-# 34    chain=dstnat action=dst-nat to-addresses=192.168.69.111 to-ports=33111 protocol=tcp in-interface=ether1 dst-port=33111
-# 1    chain=dstnat action=dst-nat to-addresses=192.168.69.111 to-ports=33111 protocol=tcp dst-address=27.131.160.106 dst-port=33111
-#
-# interface --> address=public-ip
-
 def generate_routeros_command(item, public_ip):
     to_addresses, to_ports = item['to_host'].split(':')
     routeros_command = (
@@ -16,6 +7,7 @@ def generate_routeros_command(item, public_ip):
         f"dst-port={item['from_port']}"
     )
     return routeros_command
+
 
 class FilterModule(object):
     def filters(self):
