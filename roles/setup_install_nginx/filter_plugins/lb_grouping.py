d3920eb64ffcf6def57e3ed080cee4c58a1a41ef
hitchhooker <tommi@niemi.lol>
2023-09-03 10:38:15 +0700

    add loadbalancer as conditional extension

--- roles/setup_install_nginx/filter_plugins/lb_grouping.py
+++ roles/setup_install_nginx/filter_plugins/lb_grouping.py
@@ -1,18 +1,39 @@
 # lb_grouping.py
+
 def lb_grouping(hostvars, hosts):
     """
     This filter plugin will sort and group hosts by `default_public_dns_lb`.
+    
+    Args:
+        hostvars (dict): Host variables provided by Ansible.
+        hosts (list): List of hostnames.
+        
+    Returns:
+        dict: A dictionary where keys are the first part of `default_public_dns_lb`
+        domains, and values are lists of hostnames associated with that domain.
     """
+    if not isinstance(hostvars, dict):
+        raise ValueError("hostvars must be a dictionary")
+
+    if not isinstance(hosts, list):
+        raise ValueError("hosts must be a list")
+
     result = {}
 
     for host in hosts:
-        lb_domain = hostvars[host].get('default_public_dns_lb', None)
+        if not isinstance(host, str):
+            raise ValueError(f"Invalid host name: {host}")
+
+        lb_domain = hostvars.get(host, {}).get('default_public_dns_lb')
+
         if lb_domain:
+            if not isinstance(lb_domain, str):
+                raise ValueError(
+                    f"Invalid LB domain for host {host}: {lb_domain}")
+
             key = lb_domain.split('.')[0]
-            if key in result:
-                result[key].append(host)
-            else:
-                result[key] = [host]
+            result.setdefault(key, []).append(host)
+
     return result
 
 
